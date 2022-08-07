62fe0f00ffad075d042abb07b56c31439f31fb39
Alex Manzi <alex.manzi22@gmail.com>
2022-08-07 12:09:04 -0400

    homepage template

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './Home.css';
 
 function Home() {
   return (
+    <div id="hpagemain">
     <div id="hpage">
+      <div id="leftside">
+        <h1 id="mainheading">Live Forecasts So You’ll Never Forget Your Umbrella.</h1>
+      </div>
+      <div id="rightside">
+        <div id="maincitytop">
+          
+        </div>
+        <div id="maincityinfo">
 
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
