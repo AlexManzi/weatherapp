075fbbbd48554b209ff2a91c0eb03f2b36a0564d
Alex Manzi <alex.manzi22@gmail.com>
2022-08-09 17:49:01 -0400

    no state

--- frontend/src/components/Citypage.js
+++ frontend/src/components/Citypage.js
@@ -6,7 +6,8 @@ import Hourlycard from './Hourlycard';
 
 function Citypage() {
   const location = useLocation()
-  let cityName = location.state.cityname
+  console.log(location)
+  let cityName = location.someData.cityname
   let [currentWeatherInfo, setCurrentWeatherInfo] = useState([])
   let [hourlyWeatherInfo, setHourlyWeatherInfo] = useState([])
   let [dailyWeatherInfo, setDailyWeatherInfo] = useState([])
