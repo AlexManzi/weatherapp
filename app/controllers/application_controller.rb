e1bff2d7ff716c898035565060e503809c1e11b7
Alex Manzi <alex.manzi22@gmail.com>
2022-08-09 01:37:01 -0400

    authenticity token hotfix 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::API
-    protect_from_forgery unless: -> { request.format.json? }
+    skip_before_action :verify_authenticity_token, raise: false
 end
